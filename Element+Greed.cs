6ea2d6b13eb0d246257fbace1cfb56c6e759b3d2
GlebGlushko <32998810+GlebGlushko@users.noreply.github.com>
2018-06-29 01:10:19 +0300

    Add files via upload
    Element+Greed.cs - consists solution for task3
    Implict_Enum.cs - consists solution for Task3

---
+++ Element+Greed.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greed
+{
+    class Element
+    {
+        public int n { get; set;}
+        public int c{ get; set;}   
+        public List<int> p{ get; set;}
+        public List<int> w{ get; set;}
+        public List<bool> take{ get; set;}
+        public Dictionary<KeyValuePair<int, int>, bool> E{ get; set;}
+        
+        public Element(int n, int c, List<int> p, List<int> w )
+        {
+            this.n = n;
+            this.c = c;
+            this.p = p;
+            this.w = w;
+            //this.E = E;
+            this.take = new List<bool>();
+            this.E = new Dictionary<KeyValuePair<int, int>, bool>();// else this.E = E;
+
+        }
+
+        public void AddForbidden(int x, int y)
+        {
+            
+            this.E.Add(new KeyValuePair<int, int>(x, y), true);
+            this.E.Add(new KeyValuePair<int, int>(y, x), true);
+        }
+        public override string ToString()
+        {
+            string res =  "weigts = ";
+            foreach (int i in this.w)
+                res += i.ToString() + ' ';
+            res += "\npoints = ";
+            foreach (int i in this.p)
+                res += i.ToString() + ' ';
+            return res;
+            //+this.w.ToString() + "\n points = " + this.p.ToString();
+        }
+        public int Calculate()
+        {
+            for (int j = 0; j < this.n; ++j)
+            {
+                this.take.Add(false);
+                bool flag = false;
+                int sum = 0;
+                for (int i = 0; i < j; ++i)
+                {
+                    if (this.take[i] && this.E.ContainsKey(new KeyValuePair<int, int>(i, j)))
+                    {
+                        flag = true;
+                        break;
+                    }
+                    else
+                        if (this.take[i]) sum += this.w[i];
+                }
+                if (sum + this.w[j] <= this.c && !flag) this.take[j] = true; 
+            }
+            int ans = 0;
+            for (int i = 0; i < this.n; ++i)
+                if (this.take[i]) ans += this.p[i];
+            return ans;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, c;
+            List<int> p = new List<int>();
+            List<int> w = new List<int>();
+            n = Int32.Parse(Console.ReadLine());
+            c = Int32.Parse(Console.ReadLine());
+            string[] pp = Console.ReadLine().Split(' ');
+            string[] ww = Console.ReadLine().Split(' ');
+            for (int i = 0; i < pp.Length; ++i)
+            {
+                p.Add(Int32.Parse(pp[i]));
+                w.Add(Int32.Parse(ww[i]));
+            }
+            Implict_enum check = new Implict_enum(n, c, p, w, 10);
+            int m = Int32.Parse(Console.ReadLine());
+            string[] xy;
+            for (int i = 0; i < m; ++i)
+            {
+                xy = Console.ReadLine().Split(' ');
+                check.AddForbidden(Int32.Parse(xy[0]), Int32.Parse(xy[1]));
+            }
+            Console.WriteLine(check);
+            List<bool> kk = new List<bool>();
+            kk.Add(false);
+            check.calcImplic(kk); 
+            //check.calcImplic(new List<bool>(true));
+        }
+    }
+}
---
+++ Implict_Enum.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greed
+{
+
+    class Node
+    {
+        List<int> leaf;
+        public List<bool> take { get; set; }
+        public Node(List<int> leaf, List<bool> take)
+        {
+            this.leaf = leaf;
+            this.take = take;
+           // this.F = new List<int>();
+        }
+    }
+    class Implict_enum : Element
+    {
+        int minimum;
+        List <Node> nodes;
+        public Implict_enum(int n, int c, List<int> p, List<int> w, int minimum): 
+            base(n, c, p, w)
+        {
+            this.minimum = minimum;
+            this.nodes = new List<Node>();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "\nminimum = " + this.minimum.ToString() + "\nLength of list of nodes = " + this.nodes.Count.ToString();
+        }
+
+        public void calcImplic (List<bool> t){
+            int k = t.Count;
+            if (k >= this.n) return;
+            List<bool> yes = new List<bool>();
+            yes.AddRange(t);
+            //int k = t.Count;
+            List<bool> no = new List<bool>();
+            no.AddRange(t);
+            yes.Add(true);
+            no.Add(false);
+            
+            int WsumYes = 0, WsumNo = 0;
+            int PsumYes = 0, PsumNo = 0;
+            for (int i=0;i<=k; ++i)
+            {
+                if (yes[i]) WsumYes += this.w[i];
+                if (no[i]) WsumNo += this.w[i];
+                if (yes[i]) PsumYes += this.p[i];
+                if (no[i]) PsumNo += this.p[i];
+
+            }
+            bool flag = false;
+            for (int i = 0; i < k; ++i)
+                if (this.E.ContainsKey(new KeyValuePair<int, int>(k, i)))
+                {
+                    flag = true;
+                    break;
+                }
+            Node next = new Node(null, yes);
+            for (int i=0;i<this.nodes.Count;++i)
+            {
+                Node last = new Node(null,t);
+                if (isDominate(last, next))
+                {
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) this.nodes.Add(new Node(null,yes));
+            calcImplic(yes);
+            ////////////////////////
+            
+            next = new Node(null, no);
+            for (int i = 0; i < this.nodes.Count; ++i)
+            {
+                Node last = new Node(null, t);
+                if (isDominate(last, next))
+                {
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) this.nodes.Add(new Node(null, no));
+
+
+        }
+        private bool isDominate(Node x, Node y)
+        {
+            int psum1 = 0, psum2 = 0, wsum1 = 0, wsum2 = 0;
+            for (int i = 0; i < x.take.Count; ++i)
+                if (x.take[i]) { psum1 += this.p[i]; wsum1 += this.w[i]; }
+            for (int i = 0; i < y.take.Count; ++i)
+                if (y.take[i]) { psum2 += this.p[i]; wsum2 += this.w[i]; }
+
+            return (psum1 > psum2 && wsum1 < wsum2 && createF(x).IsSubsetOf(createF(y))); 
+            
+        }
+        private HashSet<int> createF(Node x)
+        {
+            HashSet <int> res  = new HashSet<int>();
+            for (int i=0;i<x.take.Count;++i)
+            {
+                for (int j=i+1; j<x.take.Count; ++j) 
+                    if (this.E.ContainsKey(new KeyValuePair<int,int>(i,j))) res.Add(j);
+            }
+            return res;
+        } 
+    }
+}
